7856cc2a1d99c794d8d2c9e130f3dc3d916b2157
peioe <peio.etchegoin@gmail.com>
2016-04-04 18:48:53 +0200

    fixed escape bugs

--- checklogs.py
+++ checklogs.py
@@ -21,19 +21,32 @@ checklogexe_path = checklogexe_path.replace(" ", "\ ")
 def process_log(log_full_path):
     global logs_goodlist, logs_badlist, logs_ignored_list, checklogexe_path
     log_full_path = log_full_path.replace(" ", "\ ")
-    command = 'script -c "wine-development %s %s" tmpfile &> /dev/null' % (checklogexe_path, log_full_path)
+    log_full_path = log_full_path.replace("(", "\(")
+    log_full_path = log_full_path.replace("'", "\\\'")
+    log_full_path = log_full_path.replace(")", "\)")
+    log_full_path = log_full_path.replace("&", "\&")
+    
+    command = 'script -q -c "wine-development %s %s" /dev/null' % (checklogexe_path, log_full_path)
+    
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
     output = process.stdout.read()
-    output = output.replace("\x1b[?1h\x1b=Log Integrity Checker   (C) 2010 by Andre Wiethoff\r\r\n\r\r\n1. ", "")
-    output = output.replace("Script started, file is tmpfile\n", "")
-    output = output.replace("\r\r\n\x1b[?1l\x1b>Script done, file is tmpfile\n", "")
-    if output == "Log entry was modified, checksum incorrect!":
-        logs_badlist.append(log_full_path)
-    if output == "Log entry is fine!":
-        logs_goodlist.append(log_full_path)
-    if output == "Log entry has no checksum!":
+    #output = output.replace("\x1b[?1h\x1b=Log Integrity Checker   (C) 2010 by Andre Wiethoff\r\r\n\r\r\n1. ", "")
+    #output = output.replace("Script started, file is tmpfile\n", "")
+    #output = output.replace("\r\r\n\x1b[?1l\x1b>Script done, file is tmpfile\n", "")
+    #if output == "Log entry was modified, checksum incorrect!":
+    #    logs_badlist.append(log_full_path)
+    #elif output == "Log entry is fine!":
+    #    logs_goodlist.append(log_full_path)
+    #elif output == "Log entry has no checksum!":
+    #    logs_ignoredlist.append(log_full_path)
+    if "Log entry has no checksum!" in output:
         logs_ignoredlist.append(log_full_path)
-    
+    if "Log entry was modified, checksum incorrect!" in output:
+        logs_badlist.append(log_full_path)
+    elif "Log entry is fine!" in output:
+        logs_goodlist.append(log_full_path)
+    else:
+        print command
 
 def main():
     global logs_goodlist, logs_badlist, logs_ignored_list
@@ -60,6 +73,7 @@ def main():
         process_log(log)
     print "Found %d good log(s), %d edited log(s) and skipped %d log(s) (no checksum)." % (len(logs_goodlist), len(logs_badlist), len(logs_ignoredlist))
     print "Good logs:"
+    logs_goodlist.sort()
     for log in logs_goodlist:
         print log
     print "Ignored logs (no checksum):"
@@ -68,7 +82,7 @@ def main():
     print "BAD (edited) logs:"
     for log in logs_badlist:
         print log
-    os.remove("tmpfile")
+    #os.remove("tmpfile")
     
 if __name__ == '__main__':
 	main()
